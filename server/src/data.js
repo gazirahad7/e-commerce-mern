5098c6a7c9be8b58bab818e19fa01fba075eb598
gazirahad7 <65822873+gazirahad7@users.noreply.github.com>
2023-07-29 22:00:32 +0600

    Added: seed products data

--- server/src/data.js
+++ server/src/data.js
@@ -17,6 +17,50 @@ const data = {
       image: "default.png",
     },
   ],
+
+  products: [
+    {
+      name: "iphone 11",
+      slug: "iphone-11",
+      description:
+        "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here',",
+
+      price: 700.5,
+      image: "iphone11.jpg",
+      sold: 20,
+      quantity: 10,
+      shipping: 0,
+      category: "64c5348ed657b44a3cb8a2e6",
+    },
+    {
+      name: "iphone 12",
+      slug: "iphone-12",
+      description:
+        "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here',",
+
+      price: 700.5,
+      image: "iphone12.jpg",
+      quantity: 10,
+
+      sold: 10,
+      shipping: 0,
+      category: "64c5348ed657b44a3cb8a2e6",
+    },
+    {
+      name: "samsung s22",
+      slug: "samsung-s22",
+      description:
+        "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here',",
+
+      price: 900.5,
+      image: "samsung22.jpg",
+      quantity: 12,
+
+      sold: 4,
+      shipping: 0,
+      category: "64c534a2d657b44a3cb8a2e8",
+    },
+  ],
 };
 
 module.exports = { data };
